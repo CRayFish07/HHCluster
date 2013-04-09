413714c2d0070fec118239004f38c0b75608ab53
AthalberthH <ahsalberto@gmail.com>
2013-04-09 13:34:21 +0200

    Index now outs for using later the KeyValueTextInputFormat

--- Indexer/src/edu/ub/ahstfg/indexer/mapred/IndexRecordWriter.java
+++ Indexer/src/edu/ub/ahstfg/indexer/mapred/IndexRecordWriter.java
@@ -31,41 +31,38 @@ public class IndexRecordWriter implements RecordWriter<Text, IndexRecord> {
             String url = doc.getUrl();
             long[] keyFreq = doc.getKeyFreq();
             long[] termFreq = doc.getTermFreq();
-            out.writeBytes("<document>\n");
-            out.writeBytes("  <url>\n");
-            out.writeBytes("    " + url);
-            out.writeBytes("  </url>\n");
+
+            out.writeBytes(url + "\t");
             if (keyFreq != null) {
-                out.writeBytes("  <keywords>\n");
+                out.writeBytes("keywords:");
                 for (int i = 0; i < keyFreq.length; i++) {
                     out.writeBytes(keyFreq[i] + ",");
                 }
-                out.writeBytes("  </keywords>\n");
+                out.writeBytes(";");
             }
-            out.writeBytes("  <terms>\n");
+            out.writeBytes("terms:");
             for (int i = 0; i < termFreq.length; i++) {
                 out.writeBytes(termFreq[i] + ",");
             }
-            out.writeBytes("  </terms>\n");
-            out.writeBytes("</document>\n");
+            out.writeBytes(".\n");
         } else {
             FeatureDescriptor features = (FeatureDescriptor) value;
             String[] keywords = features.getKeywords();
             String[] terms = features.getTerms();
-            out.writeBytes("<features>\n");
+
+            out.writeBytes(key.toString() + "\t");
             if (keywords != null) {
-                out.writeBytes("  <keywords>\n");
+                out.writeBytes("keywords:");
                 for (int i = 0; i < keywords.length; i++) {
                     out.writeBytes(keywords[i] + ",");
                 }
-                out.writeBytes("  </keywords>\n");
+                out.writeBytes(";");
             }
-            out.writeBytes("  <terms>\n");
+            out.writeBytes("terms:");
             for (int i = 0; i < terms.length; i++) {
                 out.writeBytes(terms[i] + ",");
             }
-            out.writeBytes("  </terms>\n");
-            out.writeBytes("</features>\n");
+            out.writeBytes(".\n");
         }
     }
 }
